4b4bb92c7f7547655d6fb8517b010b7bd4730e10
Amit <133666068+Garzzzza@users.noreply.github.com>
2024-01-17 19:16:55 +0200

    CSStuff

--- src/Components/Pet.jsx
+++ src/Components/Pet.jsx
@@ -1,117 +1,146 @@
-import React, { useEffect, useContext } from "react"
-import { Link } from "react-router-dom"
-import { SignContext } from "../Context/SignContext"
-import { PetContext } from "../Context/PetContext"
-import { PetUserContext } from "../Context/PetUserContext"
-import { SpecificPet } from "../Pages/SpecificPet"
-
+import React, { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { SignContext } from "../Context/SignContext";
+import { PetContext } from "../Context/PetContext";
+import { PetUserContext } from "../Context/PetUserContext";
+import { SpecificPet } from "../Pages/SpecificPet";
 
 function Pet({ pet }) {
+  const { currentUser, getSignedUserById, token } = useContext(SignContext);
+  const { handlePetUpdate, getPetById, theSpecificPet } =
+    useContext(PetContext);
+  const {
+    addSavedPet,
+    deleteSavedPet,
+    savedPetsArray,
+    setSavedPetsArray,
+    getSavedPetsArray,
+    addAdoptedPet,
+    getAdoptedPetsArray,
+    adoptedPetsArray,
+    setAdoptedPetsArray,
+    deleteAdoptedPet,
+    fosteredPetsArray,
+    setFosteredPetsArray,
+    addFosteredPet,
+    getFosteredPetsArray,
+    deleteFosteredPet,
+  } = useContext(PetUserContext);
 
+  useEffect(() => {
+    getSignedUserById();
+  }, []);
 
+  useEffect(() => {
+    async function fetchPets() {
+      await getSavedPetsArray();
+      await getAdoptedPetsArray();
+      await getFosteredPetsArray();
+    }
+    fetchPets();
+  }, [currentUser]);
 
-    const { currentUser, getSignedUserById, token, } = useContext(SignContext)
-    const { handlePetUpdate, getPetById, theSpecificPet } = useContext(PetContext)
-    const { addSavedPet, deleteSavedPet, savedPetsArray, setSavedPetsArray, getSavedPetsArray, addAdoptedPet, getAdoptedPetsArray, adoptedPetsArray, setAdoptedPetsArray, deleteAdoptedPet,
-        fosteredPetsArray, setFosteredPetsArray, addFosteredPet, getFosteredPetsArray, deleteFosteredPet, } = useContext(PetUserContext)
+  const savedPet = savedPetsArray.some(
+    (savedPet) => savedPet.petId == pet.petId
+  );
 
-    useEffect(() => {
-        getSignedUserById();
-    }, [])
+  const savedPetByUser = savedPetsArray.some((savedPet) => {
+    return savedPet.petId == pet.petId && savedPet.userId == currentUser.userId;
+  });
 
-    useEffect(() => {
+  const fosteredPet = fosteredPetsArray.some(
+    (fosteredPet) => fosteredPet.petId == pet.petId
+  );
 
-        async function fetchPets() {
-            await getSavedPetsArray();
-            await getAdoptedPetsArray();
-            await getFosteredPetsArray();
-
-        };
-        fetchPets();
-
-    }, [currentUser]);
-
-    const savedPet = savedPetsArray.some((savedPet) => savedPet.petId == pet.petId)
-
-    const savedPetByUser = savedPetsArray.some((savedPet) => { return savedPet.petId == pet.petId && savedPet.userId == currentUser.userId });
-
-    const fosteredPet = fosteredPetsArray.some((fosteredPet) => fosteredPet.petId == pet.petId)
-
-    const fosteredPetByUser = fosteredPetsArray.some((fosteredPet) => { return fosteredPet.petId == pet.petId && fosteredPet.userId == currentUser.userId });
-
-    const adoptedPet = adoptedPetsArray.some((adoptedPet) => adoptedPet.petId == pet.petId)
-
-    const adoptedPetByUser = adoptedPetsArray.some((adoptedPet) => { return adoptedPet.petId == pet.petId && adoptedPet.userId == currentUser.userId })
-
+  const fosteredPetByUser = fosteredPetsArray.some((fosteredPet) => {
     return (
-        <div className="petDiv">
-            < Link to={"/pets/" + pet.petId} >
+      fosteredPet.petId == pet.petId && fosteredPet.userId == currentUser.userId
+    );
+  });
 
-                <div className="petCharacteristic">
-                    <img src={pet.picture} />
+  const adoptedPet = adoptedPetsArray.some(
+    (adoptedPet) => adoptedPet.petId == pet.petId
+  );
 
-                </div>
-                <div className="petCharacteristic">
-                    name: {pet.name}
-                </div>
-                <div className="petCharacteristic">
-                    adoptionStatus: {pet.adoptionStatus}
+  const adoptedPetByUser = adoptedPetsArray.some((adoptedPet) => {
+    return (
+      adoptedPet.petId == pet.petId && adoptedPet.userId == currentUser.userId
+    );
+  });
 
-                </div>
+  return (
+    <div className="petDiv">
+      <Link to={"/pets/" + pet.petId}>
+        <div className="petCharacteristic">
+          <img src={pet.picture} />
+        </div>
+        <div className="petCharacteristic">name: {pet.name}</div>
+        <div className="petCharacteristic">
+          adoptionStatus: {pet.adoptionStatus}
+        </div>
+      </Link>
+      <div className="petCharacteristic">
+        {token && !savedPetByUser && !adoptedPet && (
+          <button
+            onClick={() => {
+              addSavedPet(currentUser.userId, pet.petId);
+            }}
+          >
+            Save
+          </button>
+        )}
 
+        {token && savedPetByUser && !adoptedPet && (
+          <button
+            onClick={() => {
+              deleteSavedPet(currentUser.userId, pet.petId);
+            }}
+          >
+            UnSave
+          </button>
+        )}
 
-            </Link >
-            {
-                token && !savedPetByUser && !adoptedPet &&
-                < button onClick={() => { addSavedPet(currentUser.userId, pet.petId) }}>
-                    Save
-                </button>
-            }
+        {token && !fosteredPet && !adoptedPet && (
+          <button
+            onClick={() => {
+              addFosteredPet(currentUser.userId, pet.petId);
+            }}
+          >
+            Foster
+          </button>
+        )}
 
-            {
-                token && savedPetByUser && !adoptedPet &&
-                < button onClick={() => { deleteSavedPet(currentUser.userId, pet.petId) }}>
-                    UnSave
-                </button>
-            }
+        {token && fosteredPetByUser && !adoptedPet && (
+          <button
+            onClick={() => {
+              deleteFosteredPet(currentUser.userId, pet.petId);
+            }}
+          >
+            UnFoster
+          </button>
+        )}
 
-            {
-                token && !fosteredPet && !adoptedPet &&
-                < button onClick={() => { addFosteredPet(currentUser.userId, pet.petId) }}>
-                    Foster
-                </button>
-            }
-
-            {
-                token && fosteredPetByUser && !adoptedPet &&
-                < button onClick={() => { deleteFosteredPet(currentUser.userId, pet.petId) }}>
-                    UnFoster
-                </button>
-            }
-
-            {
-                token && !adoptedPet &&
-                <button onClick={() => {
-                    addAdoptedPet(currentUser.userId, pet.petId);
-
-                }}>
-                    Adopt
-                </button>
-            }
-            {
-                token && adoptedPetByUser &&
-                <button onClick={() => { deleteAdoptedPet(currentUser.userId, pet.petId) }}>
-                    Return
-                </button>
-            }
-
-
-
-
-        </div >
-
-    )
-
+        {token && !adoptedPet && (
+          <button
+            onClick={() => {
+              addAdoptedPet(currentUser.userId, pet.petId);
+            }}
+          >
+            Adopt
+          </button>
+        )}
+        {token && adoptedPetByUser && (
+          <button
+            onClick={() => {
+              deleteAdoptedPet(currentUser.userId, pet.petId);
+            }}
+          >
+            Return
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export { Pet }
+export { Pet };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,29 @@
-import React, { useContext, useEffect } from 'react'
-import { SignContext } from "../Context/SignContext"
-
-
+import React, { useContext, useEffect } from "react";
+import { SignContext } from "../Context/SignContext";
 
 const Home = () => {
-    const { getSignedUserById, currentUser, showSignInModal, } = useContext(SignContext)
+  const { getSignedUserById, currentUser, showSignInModal } =
+    useContext(SignContext);
 
-    useEffect(() => { getSignedUserById(); console.log(currentUser) }, [showSignInModal])
+  useEffect(() => {
+    getSignedUserById();
+    console.log(currentUser);
+  }, [showSignInModal]);
 
+  return (
+    <div>
+      <div className="welcome">
+        Welcome To The Pet Adoption Agency {currentUser.fullName}
+      </div>
+      <div className="welcomeText">In this site you can adopt pets.</div>
+      <div className="homePic">
+        <img
+          className="homePic"
+          src="https://d1g9yur4m4naub.cloudfront.net/images/Article_Images/ImageForArticle_714_16449362895935733.jpg"
+        ></img>
+      </div>
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-
-            <div className="welcome">Welcome To The Pet Adoption Agency {currentUser.fullName}</div>
-            <div className="welcomeText">
-                In this site you can adopt pets.
-            </div>
-        </div>
-
-    )
-}
-
-export default Home
+export default Home;
